8b71741d6071120c51ef9ae1d70aeced4225896b
yfb <yfb@yfbdeiMac.local>
2016-07-08 17:34:51 +0800

    s

---
+++ 练习21-2048小游戏/animation.js
@@ -0,0 +1,23 @@
+function showNumberWithAnimation(i, j, randNumber) {
+    var numberCell = $("#number-cell-" + i + "-" + j);
+    numberCell.css("background", getNumberBackgroundColor(randNumber));
+    numberCell.css("color", getNumberColor(randNumber));
+    numberCell.text(randNumber);
+    numberCell.animate({
+        width: cellSideLength,
+        height: cellSideLength,
+        top: getPosTop(i, j),
+        left: getPosLeft(i, j)
+    }, 50)
+}
+function showMove(fromx, fromy, tox, toy) {
+    var numberCell = $("number-cell-" + fromx + "-" + fromy);
+    numberCell.animate({
+        top: getPosTop(tox, toy),
+        left: getPosLeft(tox, toy)
+    }, 200);
+}
+
+function updateScore(score) {
+    $("#score").text(score);
+}
